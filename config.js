da03936a447915d02721172248aadd9e667f63fb
kimen26 <yann.ponaire@gmail.com>
2026-08-25 17:07:08 +0200

    Site : mise à jour

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration publique du site IArtcane.
-// Cle anon legacy : publique PAR DESIGN (embarquee dans le navigateur) ;
+// Cle publishable : publique PAR DESIGN (embarquee dans le navigateur) ;
 // la securite est assuree par RLS (chaque owner ne voit que ses objets).
 window.IARTCANE_CONFIG = {
   SUPABASE_URL: "https://jzgfayajezsqluahwfcd.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6Z2ZheWFqZXpzcWx1YWh3ZmNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc0ODYxMDYsImV4cCI6MjEwMzA2MjEwNn0.Mi3qK7CYVquTgCkmmHy_j6yh1oxQrhdX9wRlahqQzWw",
+  SUPABASE_ANON_KEY: "sb_publishable_v6q-QuruWckCE4KR89OIPg_GerJvy0n",
 };
